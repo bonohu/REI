1c8d510ec4c3346ae9dc52f1116b607e08d71158
Hidemasa Bono <bonohu@gmail.com>
2014-11-12 21:01:23 +0900

    script to run lftp for get the list of experiments for AOE

--- 00script/00getexfind.sh
+++ 00script/00getexfind.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-# Get list of files in arrayexpress ftp site(experiment)
+# script to run lftp to get list of files in arrayexpress ftp site(experiment)
+#
 lftp <<-END
         open ftp.ebi.ac.uk:/pub/databases/microarray/data/experiment
         find
