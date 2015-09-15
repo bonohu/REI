a951c11d799ad23b12183e26dab92f636a3504ed
Hidemasa Bono <bonohu@gmail.com>
2015-09-15 10:17:27 +0900

    modified:   10getsrameta.sh

--- 00script/10getsrameta.sh
+++ 00script/10getsrameta.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-lftp -c "open ftp://ftp.ncbi.nlm.nih.gov/sra/reports/Metadata && pget -n 8 SRA_Accessions.tab"
+#lftp -c "open ftp://ftp.ncbi.nlm.nih.gov/sra/reports/Metadata && pget -n 8 SRA_Accessions.tab"
+lftp -c "open http://sra.dbcls.jp/data && pget -n 8 sra2geo.tab"
