1a5836e584215c56bd08edda7d514b63344640c9
Hidemasa Bono <bonohu@gmail.com>
2015-09-15 11:38:10 +0900

    new file:   10bioprj2gse.prl modified:   10getsrameta.sh

--- 00script/10getsrameta.sh
+++ 00script/10getsrameta.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# get file for SRA2GEO relation from DBCLS website (generated from SRA metadata)
 
 #lftp -c "open ftp://ftp.ncbi.nlm.nih.gov/sra/reports/Metadata && pget -n 8 SRA_Accessions.tab"
 lftp -c "open http://sra.dbcls.jp/data && pget -n 8 sra2geo.tab"
