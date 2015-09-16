c62a8ab851329a83082a8e0e1219d63dbf240b63
Hidemasa Bono <bonohu@gmail.com>
2015-09-16 15:11:45 +0900

    modified:   10getsrameta.sh

--- 00script/10getsrameta.sh
+++ 00script/10getsrameta.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # get file for SRA2GEO relation from DBCLS website (generated from SRA metadata)
+# originally from XML files at 
+# ftp://ftp.ddbj.nig.ac.jp/ddbj_database/dra/fastq/
+
 
 #lftp -c "open ftp://ftp.ncbi.nlm.nih.gov/sra/reports/Metadata && pget -n 8 SRA_Accessions.tab"
 lftp -c "open http://sra.dbcls.jp/data && pget -n 8 sra2geo.tab"
